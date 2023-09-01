c7550540c9bd857a6708b741e45b955024136c18
EstebGtzMtz <esteb.gtz.mtz@hotmail.com>
2023-09-01 16:03:16 -0600

    first query using RTK query

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,11 +1,31 @@
+import { useGetTodosQuery } from "./store/apis/todosApi"
+
 export const TodoApp = () => {
+
+  const {data: todos=[], isLoading, isFetching} = useGetTodosQuery();
+  console.log({
+    todos,
+    isLoading,
+    isFetching
+  })
+
   return (
     <>
       <h1>Todos - RTL Query</h1>
       <hr />
 
-      <h4>....isLoading</h4>
-      <pre>..</pre>
+      <h4>isloading: { isLoading ? 'true' : 'false' } </h4>
+      <pre>...</pre>
+
+      <ul>
+        {
+          todos.map(todo => (
+            <li key={todo.id}>
+              {todo.title}
+            </li>
+          ))
+        }
+      </ul>
 
       <button>
         Next Todo
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { todoApi } from './apis/todosApi';
 import { counterSlice } from './slices/counter';
 import { pokemonSlice } from './slices/pokemon/PokemonSlice';
 
@@ -6,6 +7,10 @@ import { pokemonSlice } from './slices/pokemon/PokemonSlice';
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
-    pokemons: pokemonSlice.reducer
+    pokemons: pokemonSlice.reducer,
+
+    [todoApi.reducerPath]: todoApi.reducer
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware()
+    .concat(todoApi.middleware)
 })
